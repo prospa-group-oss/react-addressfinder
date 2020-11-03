6b0dcbde4d1d12165eaa07dc77dc4071de7505d9
Shashank Deshmukh <shashank.deshmukh@prospa.com>
2020-11-03 15:18:25 +1100

    adding city and suburb fields for NZ

--- src/types.ts
+++ src/types.ts
@@ -7,13 +7,16 @@ export interface AddressMeta {
   postcode: string;
   full_address: string;
   canonical_address: string; // official address recorded by aus post
+  suburb: string;
+  city: string;
 }
 
 export interface Address {
   line1: string;
   line2: string;
   suburb: string;
-  state: string;
+  state?: string;
   postcode: string;
   country: string;
+  city?: string;
 }
