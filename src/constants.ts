548016d7c39abebe60bd0cc900915c31af7e8937
Aleksandr Shtykov <aleksandr.shtykov@prospa.com>
2022-01-10 15:34:29 +1000

    PDT-9081: added playground

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,5 @@ export enum Country {
   AU = 'AU',
   NZ = 'NZ',
 }
-
 export const ContainerPrefix = 'address-container';
-
 export const AddressFinderWidgetSrc = 'https://api.addressfinder.io/assets/v3/widget.js';
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/example.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+    }),
+  ],
+};
